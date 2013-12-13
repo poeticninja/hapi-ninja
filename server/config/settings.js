de86192b83cd46bf7d5423ba0ef88fd83be21767
Saul Maddox <saul.maddox@gmail.com>
2013-12-13 09:01:01 -0600

    Update templates to have includes and base template

--- server/config/settings.js
+++ server/config/settings.js
@@ -13,7 +13,7 @@ module.exports = {
             views: {
                 path: './server/views',
                 engines: {
-                    html: 'handlebars'
+                    html: 'swig'
                 }
             }
         }
