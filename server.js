057931c0462001b7d6eb92f813c91b863fe24564
Saul Maddox <saul.maddox@gmail.com>
2014-01-20 00:40:16 -0600

    Fix controllers by passing in server object. Change to routes.

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var server = Hapi.createServer('0.0.0.0', config.port, config.hapi.options);
 // Bootstrap Hapi Server Plugins, passes the server object to the plugins
 require('./server/config/plugins')(server);
 
+// Require the routes and pass the server object.
+var routes = require('./server/config/routes')(server);
 // Add the server routes
 server.route(routes);
 
--- server/config/routes.js
+++ server/config/routes.js
@@ -3,49 +3,52 @@
 */
 var requireDirectory = require('require-directory');
 
-// Bootstrap your controllers so you dont have to load them individually. This loads them all into the controller name space. https://github.com/troygoode/node-require-directory
-var controller = requireDirectory(module, './server/controllers');
+module.exports = function(server) {
+    // Bootstrap your controllers so you dont have to load them individually. This loads them all into the controller name space. https://github.com/troygoode/node-require-directory
+    var controller = requireDirectory(module, './server/controllers');
 
-// Array of routes for Hapi
-module.exports = [
-    {
-        method: 'GET',
-        path: '/about',
-        config: controller.base.about
-    },
-    {
-        method: 'GET',
-        path: '/',
-        config: controller.base.index
-    },
-    {
-        method: 'GET',
-        path: '/{path*}',
-        config: controller.base.missing
-    },
-    {
-        method: 'GET',
-        path: '/partials/{path*}',
-        config: controller.assets.partials
-    },
-    {
-        method: 'GET',
-        path: '/images/{path*}',
-        config: controller.assets.images
-    },
-    {
-        method: 'GET',
-        path: '/css/{path*}',
-        config: controller.assets.css
-    },
-    {
-        method: 'GET',
-        path: '/js/{path*}',
-        config: controller.assets.js
-    },
-    {
-        method: 'GET',
-        path: '/bower_components/{path*}',
-        config: controller.assets.bower
-    }
-];
+    // Array of routes for Hapi
+    var routeTable = [
+        {
+            method: 'GET',
+            path: '/about',
+            config: controller.base.about
+        },
+        {
+            method: 'GET',
+            path: '/',
+            config: controller.base.index
+        },
+        {
+            method: 'GET',
+            path: '/{path*}',
+            config: controller.base.missing
+        },
+        {
+            method: 'GET',
+            path: '/partials/{path*}',
+            config: controller.assets.partials
+        },
+        {
+            method: 'GET',
+            path: '/images/{path*}',
+            config: controller.assets.images
+        },
+        {
+            method: 'GET',
+            path: '/css/{path*}',
+            config: controller.assets.css
+        },
+        {
+            method: 'GET',
+            path: '/js/{path*}',
+            config: controller.assets.js
+        },
+        {
+            method: 'GET',
+            path: '/bower_components/{path*}',
+            config: controller.assets.bower
+        }
+    ];
+    return routeTable;
+}
