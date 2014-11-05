e722af20bae593f1ecb12c40172d95398d9392d5
nuklehed <ernestcarroll@gmail.com>
2014-11-05 11:31:12 -0500

    Update plugins.js
    Added a name parameter to the pack.register method because it is required.

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -13,17 +13,21 @@ var assetOptions = require(settings.rootPath + '/assets');
 
 server.pack.register([
     {
+        name: "good",
         plugin: require("good"),
         options: goodOptions
     },
     {
+        name: "hapi-assets",
         plugin: require("hapi-assets"),
         options: assetOptions
     },
     {
+        name: "hapi-named-routes",
         plugin: require("hapi-named-routes")
     },
     {
+        name: "hapi-cache-buster",
         plugin: require("hapi-cache-buster")
     }
 ], function(err) {
