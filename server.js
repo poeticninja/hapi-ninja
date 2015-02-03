cb6c740c683d3a8e924537fbdfb1467cd71deda6
Saul Maddox <saul.maddox@gmail.com>
2015-02-02 22:56:47 -0600

    refactor start

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 /**
 * Dependencies.
 */
-var Hapi = require('hapi'),
-    config = require('./server/config/settings');
+var Hapi = require('hapi');
 
 // Create a new server
 var server = new Hapi.Server();
 
 // Setup the server with a host and port
-server.connection({host: config.host, port: config.port});
+server.connection({
+    port: parseInt(process.env.PORT, 10) || 3000,
+    host: '0.0.0.0'
+});
 
 // Setup the views engine and folder
 server.views({
@@ -21,14 +23,38 @@ server.views({
 // Export the server to be required elsewhere.
 module.exports = server;
 
-// Bootstrap Hapi Server Plugins
-require('./server/config/plugins');
-
 // Add the server routes
-server.route(require('./server/config/routes'));
+server.route(require('./server.routes.js'));
 
-//Start the server
-server.start(function() {
-    //Log to the console the host and port info
-    console.log('Server started at: ' + server.info.uri);
+
+// Load all plugins and then start the server.
+// First: community/npm plugins are loaded
+// Second: project specific plugins are loaded
+server.register([
+    {
+        register: require("good"),
+        options: {
+            opsInterval: 5000,
+            reporters: [{
+                reporter: require('good-console'),
+                args:[{ ops: '*', request: '*', log: '*', response: '*', 'error': '*' }]
+            }]
+        }
+    },
+    {
+        register: require("hapi-assets"),
+        options: require('./assets.js')
+    },
+    {
+        register: require("hapi-named-routes")
+    },
+    {
+        register: require("hapi-cache-buster")
+    }
+], function () {
+    //Start the server
+    server.start(function() {
+        //Log to the console the host and port info
+        console.log('Server started at: ' + server.info.uri);
+    });
 });
--- server.routes.js
+++ server.routes.js
@@ -4,7 +4,7 @@
 var requireDirectory = require('require-directory');
 
 // Bootstrap your controllers so you dont have to load them individually. This loads them all into the controller name space. https://github.com/troygoode/node-require-directory
-var controller = requireDirectory(module, '../controllers');
+var controller = requireDirectory(module, './server/controllers');
 
 module.exports = [
     {
